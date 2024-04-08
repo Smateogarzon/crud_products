2a725ee944a0423b51efe5761b19c2004983c259
Smateogarzon <mateogarzon1002@gmail.com>
2024-04-08 01:01:33 -0500

    feat: :fire: search bar

--- back/src/app.ts
+++ back/src/app.ts
@@ -1,5 +1,5 @@
 const express = require('express');
-// const cors = require('cors');
+const cors = require('cors');
 const morgan = require('morgan');
 const routers = require('@/routes/index');
 
@@ -7,6 +7,14 @@ const app = express();
 
 // Configuración de Morgan
 app.use(morgan('dev'));
+app.use(
+  cors({
+    origin: '*',
+    methods: ['GET', 'POST', 'PUT', 'DELETE'],
+    allowedHeaders: ['Content-Type', 'Authorization'],
+    credentials: true,
+  })
+);
 app.use(express.json());
 app.use('/', routers);
 
